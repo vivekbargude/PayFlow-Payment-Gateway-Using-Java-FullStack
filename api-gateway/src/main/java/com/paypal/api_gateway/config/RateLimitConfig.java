1decc5ba9ef66df181c12e2503ec398d788f3351
vivekbargude <vivekbargude1206@gmail.com>
2025-12-16 22:44:26 +0530

    feat: create api-gateway module with rate-limiter

---
+++ api-gateway/src/main/java/com/paypal/api_gateway/config/RateLimitConfig.java
@@ -0,0 +1,24 @@
+package com.paypal.api_gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimitConfig {
+
+    @Bean
+    public KeyResolver userKeyResolver(){
+        return exchange -> {
+            String userId = exchange.getRequest().getHeaders().getFirst("X-User-Id");
+            if (userId != null){
+                return Mono.just(userId);
+            }
+            // fallback via ip address
+            return Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+        };
+
+    }
+
+}
